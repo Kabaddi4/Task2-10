5882f28a42f411db7cc22077c5822941a0050eb1
EC2 Default User <ec2-user@ip-172-31-60-239.ec2.internal>
2021-10-15 07:46:21 +0000

    [end]update action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,8 @@
 class BooksController < ApplicationController
 
   def index
-    @books = Book.all
+    @books = Book.all #モデルから全て取得する為に複数形で宣言
+       #10/14 ここか、resourcesが原因でバリデーションがうまく実装できない。メソッドの有効な活用法を学べたのが不幸中の幸い。
   end
 
   def new
@@ -11,9 +12,9 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params) #インスタンス変数で定義
     if @book.save #if文でsaveメソッドに成功した場合の処理を記述
-      redirect_to book_path
+      redirect_to books_path
     else
-      render :new
+      render :index
     end
   end
 
@@ -27,21 +28,20 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id]) #ビューを介さない為ローカル変数で取得
-    book.update(book_params) #updateメソッドで更新
-    redirect_to books_path(book.id)
-
+    @book = Book.find(params[:id]) #ビューを介さない為ローカル変数で取得
+    @book.update(book_params) #updateメソッドで更新
+    redirect_to books_path
   end
 
   def destroy
     book = Book.find(params[:id]) #ビューを介さない為ローカル変数で取得
     book.destroy #destroyメソッドで削除
     redirect_to books_path
-
   end
+
   private
   #ストロングパラメーター
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,8 @@
 class Book < ApplicationRecord
 
-  validates :title ,presence: true
-  validates :body, presence: true
+  validates :title, presence: true
+  validates :body, presence: true   #対義語が absence: 空であるか？
+
 end
+
+#presence: 空ではないか？という意
