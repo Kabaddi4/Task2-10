82beb68be037f65d7a99a7df33b24d21945e8da0
EC2 Default User <ec2-user@ip-172-31-60-239.ec2.internal>
2021-10-16 09:31:26 +0000

    [end]validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,18 +2,15 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all #モデルから全て取得する為に複数形で宣言
-        #10/15 ここで、インスタンス@book　を違和感なく実装すれば解決？それをしない限り一生 no method error()とか突っつかれる、
-  end
-
-  def new
-    @book = Book.new
+    @book = Book.new #newアクションも兼用する事ができた。。。
   end
 
   def create
     @book = Book.new(book_params) #インスタンス変数で定義
     if @book.save #if文でsaveメソッドに成功した場合の処理を記述
-      redirect_to book_path(@book.id)  #indexに戻りたいならbooks_path showなら、book_path(@インスタンス,id)
+      redirect_to books_path#indexに戻りたいならbooks_path showなら、book_path(@インスタンス,id)
     else
+      @books = Book.all #indexを呼び出す為、必要はないが定義していないと nil class になる。
       render :index
     end
   end
@@ -28,9 +25,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    @book = Book.find(params[:id]) #ビューを介さない為ローカル変数で取得
-    @book.update(book_params) #updateメソッドで更新
-    redirect_to books_path
+    @book = Book.find(params[:id]) #インスタンス変数でモデルの情報を取得
+    if @book.update(book_params) #updateメソッドで更新
+      redirect_to books_path
+    else
+      render :edit
+    end
   end
 
   def destroy
