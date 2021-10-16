7d780fed651ce60bd6737a1a1ffaf6bc838a4299
EC2 Default User <ec2-user@ip-172-31-60-239.ec2.internal>
2021-10-16 09:53:10 +0000

    end

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,13 +2,14 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all #モデルから全て取得する為に複数形で宣言
-    @book = Book.new #newアクションも兼用する事ができた。。。
+    @book = Book.new #10/16 newアクションも兼用する事ができた。。。
   end
 
   def create
     @book = Book.new(book_params) #インスタンス変数で定義
     if @book.save #if文でsaveメソッドに成功した場合の処理を記述
-      redirect_to books_path#indexに戻りたいならbooks_path showなら、book_path(@インスタンス,id)
+      flash[:notice] = "successfully"
+      redirect_to books_path  #indexに戻りたいならbooks_path showなら、book_path(@インスタンス,id)
     else
       @books = Book.all #indexを呼び出す為、必要はないが定義していないと nil class になる。
       render :index
@@ -27,6 +28,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id]) #インスタンス変数でモデルの情報を取得
     if @book.update(book_params) #updateメソッドで更新
+      flash[:alert] = "successfully"
       redirect_to books_path
     else
       render :edit
